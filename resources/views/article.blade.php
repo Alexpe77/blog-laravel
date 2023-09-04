22f97ce8572de66cf6637e34050a86052e0d773b
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-09-04 10:43:13 +0200

    Add layout + style

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Article {{ $articleId->id }}</title>
+    <title>Article {{ $articleId }}</title>
 </head>
 <!----------------- TODO: ADD THE RIGHT THING TO DISPLAY ----------->
 <body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Articles</title>
 </head>
 <body>
+    @section('content')
     @foreach ($articles as $article)
         <article>
             <h2>{{ $article->title }}</h2>

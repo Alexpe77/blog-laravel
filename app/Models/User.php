d1c06741f1eb821699102530070363ef11f55345
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-09-05 17:06:41 +0200

    Modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($pwd) {
+        $this->attributes['password'] = bcrypt($pwd);
+    }
 }

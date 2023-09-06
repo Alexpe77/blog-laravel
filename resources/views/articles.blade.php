cfb900e4d5ede43b5cd903951f0e04b831972885
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-09-06 09:54:51 +0200

    Add validation to sign up

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
     <title>My Blog</title>
 </head>
-<body>
 
-<h1>Welcome to my blog!</h1>
+<body>
+    <div class="top_block">
+        <h1>Welcome to my blog!</h1>
+        <nav><a href="/signup">Sign Up</a>
+    <a href="/login">Login</a></nav>
+    </div>
 
     @foreach ($articles as $article)
-        <article>
-            <h2><a href="{{ url('/article', ['id' => $article->id]) }}">{{ $article->title }}</a></h2>
-            <p>Written by {{ $article->author }}</p>
-            <div>{{ $article->content }}</div>
-            <p>Publish date {{ $article->publish_date }}</p>
-        </article>
+    <article>
+        <h2><a href="{{ url('/article', ['id' => $article->id]) }}">{{ $article->title }}</a></h2>
+        <p>Written by {{ $article->author }}</p>
+        <div>{{ $article->content }}</div>
+        <p>Publish date {{ $article->publish_date }}</p>
+    </article>
     @endforeach
 
     <div class="create">
         <p>Click <a href="/Blog-Laravel/public/create">here</a> to create a new article</p>
     </div>
 </body>
+
 </html>
--- resources/views/signup/create.blade.php
+++ resources/views/signup/create.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
 </head>
+
 <body>
     <h2>Welcome</h2>
     <h3>Let's sign up quickly</h3>
@@ -12,14 +14,27 @@
     <form method="POST" action="/signup">
         @csrf
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" required>
+        <input type="text" name="name" id="name" value="{{ old('name') }}" required>
+        @error('name')
+        <p>{{ $message }}</p>
+        @enderror
         <label for="username">Username</label>
-        <input type="text" name="username" id="username" required>
+        <input type="text" name="username" id="username" value="{{ old('username') }}" required>
+        @error('username')
+        <p>{{ $message }}</p>
+        @enderror
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" required>
+        <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+        @error('email')
+        <p>{{ $message }}</p>
+        @enderror
         <label for="password">Password</label>
         <input type="password" name="password" id="password" required>
+        @error('password')
+        <p>{{ $message }}</p>
+        @enderror
         <button type="submit">Submit</button>
     </form>
 </body>
+
 </html>

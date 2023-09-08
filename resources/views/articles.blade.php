8a0db71c65e9aa89d1b83b7612f9bca9d7c14d64
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-09-08 10:25:37 +0200

    Modify title style

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>My Blog</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <title>Blog</title>
 </head>
 
 <body>
-    <div class="top_block d-flex justify-content-between align-items-center">
-        <h1 class="mx-auto">Welcome to my blog!</h1>
-        <nav class="d-flex">
-            <a href="/Blog-Laravel/public/signup" class="btn btn-primary mx-1">Sign Up</a>
-            <a href="/Blog-Laravel/public/login" class="btn btn-primary mx-1">Login</a>
-        </nav>
+    <div class="top_block d-flex justify-content-between align-items-center bg-light custom-bg shadow-sm">
+        <h1 class="mx-auto mt-3 fw-bold" style="font-family: 'Lato', sans-serif;font-size: 36px;">What do you want to read today ?</h1>
+        <div class="right-section mt-3">
+            <a href="/Blog-Laravel/public/signup" class="btn btn-outline-primary mx-1 mb-2 d-block">Sign Up</a>
+            <a href="/Blog-Laravel/public/login" class="btn btn-outline-primary mx-1 mb-2 d-block">Login</a>
+        </div>
     </div>
     @if (session()->has('success'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show" class="fixed btn btn-primary">
@@ -22,17 +25,17 @@
     </div>
     @endif
 
-    <div class="container">
+    <div class="container mt-4">
     <div class="row">
         @foreach ($articles as $article)
-        <div class="col-md-4 g-2">
-            <div class="card h-100 l-100" style="width: 19rem;">
+        <div class="col-md-12 mb-3">
+            <div class="card l-100" style="width: 80%;">
                 <img src="{{ asset('img/' . $article->picture) }}" class="card-img-top img-fluid" alt="Picture of an open book">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $article->title }}</h5>
+                <div class="card-body text-center shadow">
+                    <h5 class="card-title" style="font-size: 22px;">{{ $article->title }}</h5>
                     <p class="card-text">by {{ $article->author }}</p>
                     <p class="card-text">Publish date {{ $article->publish_date }}</p>
-                    <a href="{{ url('/article', ['id' => $article->id]) }}" class="btn btn-primary">Read More</a>
+                    <a href="{{ url('/article', ['id' => $article->id]) }}" class="btn btn-primary mx-auto shadow-sm">Read More</a>
                 </div>
             </div>
         </div>
